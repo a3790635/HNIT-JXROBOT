60743edb44d1fd60636cce9b46b2b138e961ff38
小荣 <yanxiangrong_2@outlook.com>
2021-07-22 16:16:22 +0800

    添加注释

--- InertialSensor.py
+++ InertialSensor.py
@@ -6,10 +6,10 @@ from naoqi import ALProxy
 IP = "192.168.1.107"
 PORT = 9559
 
-period = 200
+period = 200   # 刷新间隔 200毫秒
 memoryProxy = ALProxy("ALMemory", IP, PORT)
 sonarProxy = ALProxy("ALSonar", IP, PORT)
-sonarProxy.subscribe("Test_InertialSensor", period, 0.0)
+sonarProxy.subscribe("Test_InertialSensor", period, 0.0)  # 订阅
 
 memValueX = "Device/SubDeviceList/InertialSensor/AngleX/Sensor/Value"
 memValueY = "Device/SubDeviceList/InertialSensor/AngleY/Sensor/Value"
@@ -18,11 +18,11 @@ memValueZ = "Device/SubDeviceList/InertialSensor/AngleZ/Sensor/Value"
 print
 try:
     while True:
-        time.sleep(period / 1000)
+        time.sleep(period / 1000)  # 每次循环要停一下，降低性能消耗
         valX = memoryProxy.getData(memValueX)
         valY = memoryProxy.getData(memValueY)
         valZ = memoryProxy.getData(memValueZ)
         print "\rAngleX = {}, AngleY = {}, AngleZ = {}".format(valX, valY, valZ),
 
 finally:
-    sonarProxy.unsubscribe("Test_InertialSensor")
+    sonarProxy.unsubscribe("Test_InertialSensor")  # 取消订阅
